a75e465ab93b665789d88deff32e84119a5041a9
Bjoern Baumeier <baumeier@votca.org>
2021-01-20 19:43:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,20 +6,20 @@ from setuptools import find_packages, setup
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 version = {}
-with open(os.path.join(HERE, 'votcapytools', '__version__.py')) as f:
+with open(os.path.join(HERE, 'PyVOTCA', '__version__.py')) as f:
     exec(f.read(), version)
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='votcapytools',
+    name='PyVOTCA',
     version=version,
     description="Generic tools to interact with Votca using Python",
     long_description=readme + '\n\n',
     author="Bjoern Baumeier",
     author_email='',
-    url='https://github.com/https://github.com/votca/votcapytools/votcapytools',
+    url='https://github.com/votca/PyVOTCA/PyVOTCA',
     packages=find_packages(),
     include_package_data=True,
     license="Apache Software License 2.0",
@@ -35,7 +35,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'xtp_gradient=votcapytools.xtp_gradient:main',
+            'xtp_gradient=PyVOTCA.xtp_gradient:main',
         ]
     },
 
