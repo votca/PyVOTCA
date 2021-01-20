63e75faf552550aca7517791fa8b503f05d72530
Bjoern Baumeier <baumeier@votca.org>
2021-01-20 12:53:47 +0100

    start making separate modules

--- votcapytools/numerical_gradient.py
+++ votcapytools/numerical_gradient.py
@@ -1,68 +1,14 @@
 """Numerical gradient."""
 import copy
-import numbers as np
+import numpy as np
 import os
 from collections import namedtuple
 from itertools import cycle
 
-import h5py
+from .coordinate_io import CoordinateIO as cio
+from .wrapper import VotcaWrapper as votca
 
-__all__ = ["NumericalGradient", "read_xyz"]
-
-""" XYZ FILES """
-
-
-def read_xyz(fin):
-    """ read a xyz file from file handle
-    Parameters
-    ----------
-    fin : file handle
-        file to read from
-    Returns
-    -------
-    fin : open file
-    xyz : namedtuple
-        returns a named tuple with coords, title and list of atomtypes.
-    See Also
-    --------
-    write_xyz
-    """
-    natoms = int(fin.readline())
-    title = fin.readline()[:-1]
-    coords = np.zeros([natoms, 3], dtype="float64")
-    atomtypes = []
-    for x in coords:
-        line = fin.readline().split()
-        atomtypes.append(line[0])
-        x[:] = list(map(float, line[1:4]))
-
-    return namedtuple("XYZFile", ["coords", "title", "atomtypes"])(coords, title, atomtypes)
-
-
-def write_xyz(fout, coords, title="", atomtypes=("A",)):
-    """ write a xyz file from file handle
-    Writes coordinates in xyz format. It uses atomtypes as names. The list is
-    cycled if it contains less entries than there are coordinates,
-    One can also directly write xyz data which was generated with read_xyz.
-    >>> xx = read_xyz("in.xyz")
-    >>> write_xyz(open("out.xyz", "w"), *xx)
-    Parameters
-    ----------
-    fout : an open file
-    coords : np.array
-        array of coordinates
-    title : title section, optional
-        title for xyz file
-    atomtypes : iteratable
-        list of atomtypes.
-    See Also
-    --------
-    read_xyz
-    """
-    fout.write("%d\n%s\n" % (coords.size / 3, title))
-    for x, atomtype in zip(coords.reshape(-1, 3), cycle(atomtypes)):
-        fout.write("%s %.18g %.18g %.18g\n" % (atomtype, x[0], x[1], x[2]))
-
+__all__ = ["NumericalGradient"]
 
 """**************************************************************
 * PART I: Functions to run a simulation for all electric fields *
@@ -74,17 +20,6 @@ def mkfldr(path):
         os.mkdir(path)
     except OSError:
         print("Creation of the directory %s failed" % path)
-
-
-def run_votca(atom, direction, name, case, threads):
-    """ Runs VOTCA and moves results to the experiments folder """
-    name_disp = "{}_at{}_dir{}_{}".format(
-        str(name), str(atom), str(direction), str(case))
-    votcacmd = "xtp_tools -e dftgwbse -o dftgwbse.xml -n {} -t {} > ./experiments/logfile_{}.log".format(
-        str(name_disp), str(threads), str(name_disp))
-    os.system(votcacmd)
-    os.replace('{}.orb'.format(str(name_disp)),
-               './experiments/{}.orb'.format(str(name_disp)))
 
 
 """**********************************************************
@@ -104,7 +39,7 @@ class NumericalGradient:
         # Read XYZ
         filename = "{}.xyz".format(str(name))
         xyzfile = open(filename)
-        xyz = read_xyz(xyzfile)
+        xyz = cio.read_xyz(xyzfile)
         xyzfile.close()
 
         # create a folder to contain all the results from the different experiments
@@ -120,11 +55,11 @@ class NumericalGradient:
                 xyzfilename_plus = "{}_at{}_dir{}_plus.xyz".format(
                     str(name), str(atom), str(coordinate))
                 xyzfile_plus = open(xyzfilename_plus, "w")
-                write_xyz(xyzfile_plus, *xyz_plus)
+                cio.write_xyz(xyzfile_plus, *xyz_plus)
                 xyzfile_plus.close()
                 print("Running XTP for displacement +{} for atom {} in direction {}".format(
                     str(dr), str(atom), str(coordinate)))
-                run_votca(atom, coordinate, name, "plus", threads)
+                votca.run(atom, coordinate, name, "plus", threads)
 
                 # displacement minus
                 xyz_minus = copy.deepcopy(xyz)
@@ -132,40 +67,13 @@ class NumericalGradient:
                 xyzfilename_minus = "{}_at{}_dir{}_minus.xyz".format(
                     str(name), str(atom), str(coordinate))
                 xyzfile_minus = open(xyzfilename_minus, "w")
-                write_xyz(xyzfile_minus, *xyz_minus)
+                cio.write_xyz(xyzfile_minus, *xyz_minus)
                 xyzfile_minus.close()
                 print("Running XTP for displacement -{} for atom {} in direction {}".format(
                     str(dr), str(atom), str(coordinate)))
-                run_votca(atom, coordinate, name, "minus", threads)
+                votca.run(atom, coordinate, name, "minus", threads)
 
-    def getEnergiesFromOrbFile(self, kind, filename, level):
-        """ Read the energies from the orb file for a certain kind of particle/excitation.
-
-        OUTPUT: numpy array with the energies of the particle/excitation kind.
-        """
-        nameOrbFile = filename
-        f = h5py.File(nameOrbFile, 'r')
-        orb = f['QMdata']
-        occupied_levels = int(orb.attrs['occupied_levels'])
-
-        total_energy = orb.attrs['qm_energy']
-        if(kind == 'BSE_singlet' or kind == 'BSE_triplet'):
-            return(total_energy+orb[kind]['eigenvalues'][level])
-        elif (kind == 'QPdiag'):
-            if (level < occupied_levels):
-                return(total_energy - orb[kind]['eigenvalues'][level])
-            else:
-                return(total_energy + orb[kind]['eigenvalues'][level])
-        elif (kind == 'QPpert'):
-            if (level < occupied_levels):
-                return(total_energy - orb['QPpert_energies'][level])
-            else:
-                return(total_energy + orb['QPpert_energies'][level])
-        elif (kind == 'dft_tot'):
-            return total_energy
-        else:
-            print("Invalid kind!")
-            exit()
+    
 
     def getGradient(self, kind, energyLevel=None):
         """ Computes the gradient for a particle/excitation kind.
@@ -184,11 +92,11 @@ class NumericalGradient:
                 # get energies from files
                 filename = "./experiments/{}_at{}_dir{}_{}.orb".format(
                     str(self.name), str(atom), str(coordinate), "plus")
-                Eplus = self.getEnergiesFromOrbFile(
+                Eplus = votca.getEnergies(
                     kind, filename, energyLevel)
                 filename = "./experiments/{}_at{}_dir{}_{}.orb".format(
                     str(self.name), str(atom), str(coordinate), "minus")
-                Eminus = self.getEnergiesFromOrbFile(
+                Eminus = votca.getEnergies(
                     kind, filename, energyLevel)
 
                 # Compute derivative
---
+++ votcapytools/wrapper.py
@@ -0,0 +1,59 @@
+import os
+import h5py
+
+__all__ = ["VotcaWrapper"]
+
+
+class VotcaWrapper:
+
+    def __init__(self): pass
+
+    # just runs xtp_tools with CMDline call, expects
+    # - an xyz file xyzname.xyz
+    # - the dftgwbse.xml options file
+    # is present in the same directory
+    def run(self, xyzname, threads=1, jobname="dftgwbse", jobdir="./"):
+        """ Runs VOTCA and moves results a job folder, if requested """
+        #name_disp = "{}_at{}_dir{}_{}".format(str(name), str(atom), str(direction), str(case))
+        votcacmd = "xtp_tools -e dftgwbse -o dftgwbse.xml -n {} -t {} > {}{}.log".format(str(xyzname), str(threads), str(jobdir), str(jobname))
+        os.system(votcacmd)
+        # copy orbfile, if jobdir is not default
+        if (jobdir != "./"):
+            os.replace('{}.orb'.format(str(xyzname)),'{}{}.orb'.format(str(jobdir),str(xyzname)))
+
+    # Reads energies from an existing HDF5 orb file
+    def getEnergies(self, kind, filename, level):
+        """ Read the energies from the orb file for a certain kind of particle/excitation.
+
+        OUTPUT: numpy array with the energies of the particle/excitation kind.
+        """
+        nameOrbFile = filename
+        f = h5py.File(nameOrbFile, 'r')
+        orb = f['QMdata']
+        occupied_levels = int(orb.attrs['occupied_levels'])
+
+        total_energy = orb.attrs['qm_energy']
+        if(kind == 'BSE_singlet' or kind == 'BSE_triplet'):
+            return(total_energy+orb[kind]['eigenvalues'][level])
+        elif (kind == 'QPdiag'):
+            if (level < occupied_levels):
+                return(total_energy - orb[kind]['eigenvalues'][level])
+            else:
+                return(total_energy + orb[kind]['eigenvalues'][level])
+        elif (kind == 'QPpert'):
+            if (level < occupied_levels):
+                return(total_energy - orb['QPpert_energies'][level])
+            else:
+                return(total_energy + orb['QPpert_energies'][level])
+        elif (kind == 'dft_tot'):
+            return total_energy
+        else:
+            print("Invalid kind!")
+            exit()
+
+
+
+
+
+
+
