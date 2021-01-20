9c23b54223318eb9ea06430ca8a147e5f84e8c18
Bjoern Baumeier <b.baumeier@tue.nl>
2021-01-20 23:46:35 +0100

    forgot init

--- PyVOTCA/__init__.py
+++ PyVOTCA/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 from .__version__ import __version__
 from .numerical_gradient import NumericalGradient
-from .wrapper import VOTCA
+from .wrapper import XTP
 from .molecule import Molecule
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
