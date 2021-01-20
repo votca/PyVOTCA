0c8396a2a5d3a23127e1bbfef2014cfb5de17b6d
felipez <tifonzafel@gmail.com>
2021-01-20 11:36:10 +0100

    removed unused badges from readme

--- tests/test_gradient.py
+++ tests/test_gradient.py
@@ -1,1 +1,6 @@
-from votcapytools import numerical_gradient
+from votcapytools.numerical_gradient import NumericalGradient
+
+
+def test_instantiation():
+    """Instantiate the gradient."""
+    NumericalGradient(0.001, 2, "H20")
