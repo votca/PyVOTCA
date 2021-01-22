859819426b7f4a088745685aaba1643350a16c78
Bjoern Baumeier <b.baumeier@tue.nl>
2021-01-22 16:16:50 +0100

    test for add atom

--- tests/test_molecule.py
+++ tests/test_molecule.py
@@ -1,6 +1,7 @@
 from PyVOTCA.molecule import Molecule
 from .utils import PATH_TEST
 from pathlib import Path
+import numpy as np
 
 
 def test_molecule(tmp_path: Path):
@@ -15,3 +16,11 @@ def test_molecule(tmp_path: Path):
     file_name = tmp_path / "test.xyz"
     mol.writeXYZfile(file_name)
     assert file_name.exists()
+
+def test_atom_add():
+    """"Check that atoms are added properly."""
+    mol = Molecule()
+    mol.add_atom("O", 1.2,1.44,5.32)
+    assert mol.elements[0] == "O"
+    assert np.allclose(mol.coordinates[0], [1.2, 1.44, 5.32])
+
