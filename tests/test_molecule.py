89e66639aa7bde2f1a669b5f2dc1ad790054776c
Bjoern Baumeier <b.baumeier@tue.nl>
2021-01-22 23:16:34 +0100

    exception testing 3

--- tests/test_molecule.py
+++ tests/test_molecule.py
@@ -77,4 +77,4 @@ class MyTestCase(unittest.TestCase):
  
     def test_total_energies_exceptions(self):
         mol=Molecule()
-        self.assertRaises(mol.getDFTEnergy(),Exception)
+        self.assertRaises(Exception,mol.getDFTEnergy())
