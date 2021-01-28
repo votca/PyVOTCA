62e40ff57a4d4da274064c4029b75a8172344f47
Bjoern Baumeier <b.baumeier@tue.nl>
2021-01-28 11:09:41 +0100

    example for gradient

---
+++ examples/run_energy.py
@@ -0,0 +1,33 @@
+from PyVOTCA import XTP
+from PyVOTCA import Molecule
+from PyVOTCA import Visualization
+
+
+# define a molecule
+mol = Molecule()
+
+# make it by hand
+mol.add_atom("C", 0, 0, 0)
+mol.add_atom("O", 1.2, 0, 0)
+
+# or read it from existing file
+# mol.readXYZfile('CO.xyz')
+
+# get a XTP object
+votca = XTP(mol)
+# change basis sets to a smaller one
+votca.options['basisset']='def2-svp'
+votca.options['auxbasisset']='aux-def2-svp'
+
+# run for the molecule
+# votca.run(mol)
+
+# only needed, if no run was performed but an existing HDF5 is read
+votca.getEnergies('example.orb')
+
+# Getting the plotting functions
+viz = Visualization(votca)
+# plotting QP corrections
+# viz.plotQPcorrections()
+# plotting absorption spectrum
+viz.plotAbsorptionGaussian()
---
+++ examples/run_gradient.py
@@ -0,0 +1,28 @@
+rom PyVOTCA import XTP
+from PyVOTCA import Molecule
+from PyVOTCA import NumericalGradient
+import numpy as np
+
+
+# define a molecule
+mol=Molecule()
+
+### make it by hand
+mol.add_atom("C", 0,0,0)
+mol.add_atom("O", 1.3,0.0,0.0)
+
+### get a XTP object
+votca=XTP(mol)
+#### this allows to change all options
+#votca.options['functional'] = 'PBE'
+votca.options['basisset']='def2-svp'
+votca.options['auxbasisset']='aux-def2-svp'
+
+# run for the molecule it its geometry
+votca.run()
+
+# calculate a DFT-GWBSE gradient at the geometry
+grad=NumericalGradient(votca,dr=0.001)
+grad.getGradient('BSE_singet',0)
+
+print(mol.getGradient())
